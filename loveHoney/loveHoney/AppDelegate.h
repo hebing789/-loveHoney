a50ef3e29a116cb7e51a3d4f52a831026648e929
1101918842@qq.com <1101918842@qq.com>
2016-10-14 22:07:04 +0800

    文件夹目录搭建

---
+++ loveHoney/loveHoney/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  loveHoney
+//
+//  Created by hebing on 16/10/14.
+//  Copyright © 2016年 hebing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

f0ffc4ebf3f6a80ddeba8a06c911c195bd09644e
deepindo <732872042@qq.com>
2016-10-15 17:32:58 +0800

    自定义titleView

---
+++ loveHoney/loveHoney/Classes/Views/ProductKindView/DDTitleView.h
@@ -0,0 +1,15 @@
+//
+//  DDTitleView.h
+//  loveHoney
+//
+//  Created by deepindo on 2016/10/15.
+//  Copyright © 2016年 hebing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDTitleView : UIView
+
++(instancetype)ddTitleView;
+
+@end
---
+++ loveHoney/loveHoney/Classes/Views/ProductKindView/DDTitleView.m
@@ -0,0 +1,25 @@
+//
+//  DDTitleView.m
+//  loveHoney
+//
+//  Created by deepindo on 2016/10/15.
+//  Copyright © 2016年 hebing. All rights reserved.
+//
+
+#import "DDTitleView.h"
+
+@interface DDTitleView ()
+
+@property (weak, nonatomic) IBOutlet UILabel *deliveryLabel;
+
+
+@end
+
+@implementation DDTitleView
+
++(instancetype)ddTitleView{
+
+    return [[NSBundle mainBundle]loadNibNamed:@"DDTitleView" owner:self options:nil].lastObject;
+}
+
+@end

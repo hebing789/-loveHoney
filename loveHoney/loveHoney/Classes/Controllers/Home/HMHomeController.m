b884c983bfa53ec98a02c9e564e22a46b604a452
黄凯 <136418206@qq.com>
2016-10-15 17:06:52 +0800

    首页轮播器雏形

--- loveHoney/loveHoney/Classes/Controllers/Home/HMHomeController.m
+++ loveHoney/loveHoney/Classes/Controllers/Home/HMHomeController.m
@@ -8,7 +8,9 @@
 
 #import "HMHomeController.h"
 
-@interface HMHomeController ()
+@interface HMHomeController () <UITableViewDataSource,UITableViewDelegate>
+
+@property (nonatomic,weak)UITableView *tableView;
 
 @end
 
@@ -16,24 +18,68 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.view.backgroundColor=[UIColor blueColor];
     
-    // Do any additional setup after loading the view.
+    [self setupTableView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupTableView{
+    
+    UITableView *tableView = [[UITableView alloc] init];
+    self.tableView = tableView;
+    self.view = tableView;
+    
+    [self setupScroll];
+    
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+- (void)setupScroll{
+    //轮播器框架
+    
+    CGRect frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 150);
+    
+    NSString *img1 = @"http://i3.download.fd.pchome.net/t_960x600/g1/M00/07/09/oYYBAFMv8q2IQHunACi90oB0OHIAABbUQAAXO4AKL3q706.jpg";
+    NSString *img2 = @"http://images.weiphone.net/attachments/photo/Day_120308/118871_91f6133116504086ed1b82e0eb951.jpg";
+    NSString *img3 = @"http://benyouhuifile.it168.com/forum/macos/attachments/month_1104/110425215921926a173e0f728e.jpg";
+    NSString *img4 = @"http://benyouhuifile.it168.com/forum/macos/attachments/month_1104/1104241737046031b3a754f783.jpg";
+    NSString *img5 = @"http://2b.zol-img.com.cn/product/68/951/cerUrKWCmHCnU.jpg";
+    
+    NSMutableArray *imageURLStrings = [NSMutableArray array];
+    [imageURLStrings addObject:img1];
+    [imageURLStrings addObject:img2];
+    [imageURLStrings addObject:img3];
+    [imageURLStrings addObject:img4];
+    [imageURLStrings addObject:img5];
+    
+    SDCycleScrollView *cycleScrollView = [SDCycleScrollView cycleScrollViewWithFrame:frame delegate:self placeholderImage:[UIImage imageNamed:@"grey1"]];
+    cycleScrollView.imageURLStringsGroup = imageURLStrings;
+    
+    cycleScrollView.pageControlAliment = SDCycleScrollViewPageContolAlimentRight;
+    cycleScrollView.currentPageDotImage = [UIImage imageNamed:@"v2_home_cycle_dot_selected"];
+    cycleScrollView.pageDotImage = [UIImage imageNamed:@"v2_home_cycle_dot_normal"];
+    
+    self.tableView.tableHeaderView = cycleScrollView;
+
 }
-*/
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 3;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"" forIndexPath:indexPath];
+    return cell;
+}
+
+
+
 
 @end

54f4ac40edc21ce7b52674169f95fb1bf9a82ac5
Flynn_Lee <flynn@qq.com>
2016-10-16 15:24:54 +0800

    用户信息界面搭建完成

--- loveHoney/loveHoney/Classes/Controllers/MyMenuAndMessage/myMeassage/HMMyMessageController.m
+++ loveHoney/loveHoney/Classes/Controllers/MyMenuAndMessage/myMeassage/HMMyMessageController.m
@@ -7,9 +7,11 @@
 //
 
 #import "HMMyMessageController.h"
-
+#import "HMUserInformationController.h"
+#import "HMSystemInformationController.h"
 @interface HMMyMessageController ()
-
+@property (nonatomic,strong)UIView *tableViewUser;
+@property (nonatomic,strong)UITableView *tableViewSystem;
 @end
 
 @implementation HMMyMessageController
@@ -17,7 +19,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    UIView *tableViewUser = [[UIView alloc]init];
+    UITableView *tableViewSystem = [[UITableView alloc]init];
+    self.tableViewUser = tableViewUser;
+    self.tableViewSystem = tableViewSystem;
+    tableViewUser.backgroundColor = [UIColor whiteColor];
+//    tableViewSystem.backgroundColor = [UIColor blueColor];
+    
+    self.view = self.tableViewSystem;
+    self.tableViewSystem.hidden = NO;
+    self.tableViewUser.hidden = YES;
+    
     [self createTopView];
+    
+    [self createTableUser];
+}
+
+- (void)createTableUser
+{
+    UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"v2_my_message_empty"]];
+    
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 100)];
+    
+    label.text = @"~~~并没有消息~~~";
+    
+    label.textAlignment = NSTextAlignmentCenter;
+    
+    label.textColor = [UIColor darkGrayColor];
+    
+    label.font = [UIFont systemFontOfSize:20];
+    
+    imageView.center = CGPointMake([UIScreen mainScreen].bounds.size.width/2, [UIScreen mainScreen].bounds.size.height / 2 - 65);
+    
+    label.center = CGPointMake([UIScreen mainScreen].bounds.size.width/2, [UIScreen mainScreen].bounds.size.height / 2);
+    
+    [self.tableViewUser addSubview:imageView];
+    
+    [self.tableViewUser addSubview:label];
+    
+    
+    
 }
 
 - (void)createTopView
@@ -41,8 +82,34 @@
     NSDictionary *colorAttr = [NSDictionary dictionaryWithObject:greenColor forKey:UITextAttributeTextColor];
     [topSegmentedControl setTitleTextAttributes:colorAttr forState:UIControlStateNormal];
     
+    [topSegmentedControl addTarget:self action:@selector(changeView:) forControlEvents:UIControlEventValueChanged];
+    
+    topSegmentedControl.selectedSegmentIndex = 0;
+    
     [self.navigationItem setTitleView: topSegmentedControl];
+    
+   
 }
 
-
+- (void)changeView:(UISegmentedControl *)seg
+{
+    NSInteger index = seg.selectedSegmentIndex;
+    
+    switch (index) {
+        case 0:
+            self.view = self.tableViewSystem;
+            self.tableViewSystem.hidden = NO;
+            self.tableViewUser.hidden = YES;
+            break;
+            
+        case 1:
+            self.view = self.tableViewUser;
+            self.tableViewSystem.hidden = YES;
+            self.tableViewUser.hidden = NO;
+            break;
+            
+        default:
+            break;
+    }
+}
 @end

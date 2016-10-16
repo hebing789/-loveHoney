215557731a5947462b9a6f06ae7ea3e2d5019d96
gaojlieMan <401633128@qq.com>
2016-10-16 10:10:33 +0800

    冲突解决

---
+++ loveHoney/loveHoney/Classes/Controllers/shopping/HMShopingSucessCell/HMTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  HMTableViewCell.m
+//  loveHoney
+//
+//  Created by gaojie on 16/10/15.
+//  Copyright © 2016年 hebing. All rights reserved.
+//
+
+#import "HMTableViewCell.h"
+
+@implementation HMTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+
+
+
+
+
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

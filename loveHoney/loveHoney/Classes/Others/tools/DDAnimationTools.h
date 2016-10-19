4abf86bd9f44c635978b6dea55987621b8ab0b84
deepindo <732872042@qq.com>
2016-10-19 20:53:48 +0800

    添加购物车动画, 以及tabBar-bageValue变化

---
+++ loveHoney/loveHoney/Classes/Others/tools/DDAnimationTools.h
@@ -0,0 +1,25 @@
+//
+//  DDParabolaAnimationTools.h
+//  loveHoney
+//
+//  Created by deepindo on 2016/10/19.
+//  Copyright © 2016年 hebing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^animationFinishedBlock)(BOOL finish);
+
+@interface DDAnimationTools : NSObject
+
+@property(nonatomic,copy)animationFinishedBlock animationFinishBlock;
+
+@property(nonatomic,strong)CALayer *layer;
+
++ (instancetype)sharedTool;
+
+- (void)startAnimationWithView:(UIView *)view startRect:(CGRect)startRect endRect:(CGPoint)endPoint finish:(animationFinishedBlock)completion;
+
++(void)shakeAnimation:(UIView *)shakeView;
+
+@end

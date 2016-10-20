51822ce4c5c3cf431ca156f3b04b8fd16482376a
1101918842@qq.com <1101918842@qq.com>
2016-10-21 00:54:44 +0800

    清空缓存的实现,没有实现缓存自动计算

--- loveHoney/loveHoney/Classes/Controllers/Me/setting/HMSettingController.m
+++ loveHoney/loveHoney/Classes/Controllers/Me/setting/HMSettingController.m
@@ -116,12 +116,18 @@ static NSString* cellId = @"HMSettingContro";
             
         }else{
             
+          
+//            [[self class]clearCache:pathAddress];
+            
+            [self clear];
+            
             [SVProgressHUD showWithStatus:@"清理缓存"];
             UITableViewCell* cell =  [self.tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
             
             cell.textLabel.text = @"清理缓存";
             UILabel* aceView = [[UILabel alloc]init];
-            aceView.text = @"0M";
+//            aceView.text =[NSString stringWithFormat:@"%f", [[self class]folderSizeAtPath:pathAddress] ];
+            aceView.text =@"0M";
             aceView.textColor = [UIColor darkGrayColor];
             aceView.textAlignment = NSTextAlignmentCenter;
             [aceView sizeToFit];
@@ -148,7 +154,106 @@ static NSString* cellId = @"HMSettingContro";
          
      }
     
+    
+    
    
 }
+////计算单个文件大小
+//
+//+(float)fileSizeAtPath:(NSString *)path{
+//    NSFileManager *fileManager=[NSFileManager defaultManager];
+//    if([fileManager fileExistsAtPath:path]){
+//        long long size=[fileManager attributesOfItemAtPath:path error:nil].fileSize;
+//        return size/1024.0/1024.0;
+//    }
+//    return 0;
+//}
+//
+////计算目录大小
+//
+//+(float)folderSizeAtPath:(NSString *)path{
+//    NSFileManager *fileManager=[NSFileManager defaultManager];
+//    float folderSize;
+//    if ([fileManager fileExistsAtPath:path]) {
+//        NSArray *childerFiles=[fileManager subpathsAtPath:path];
+//        for (NSString *fileName in childerFiles) {
+//            NSString *absolutePath=[path stringByAppendingPathComponent:fileName];
+//            folderSize +=[FileService fileSizeAtPath:absolutePath];
+//        }
+//        　　　　　//SDWebImage框架自身计算缓存的实现
+//        folderSize+=[[SDImageCache sharedImageCache] getSize]/1024.0/1024.0;
+//        return folderSize;
+//    }
+//    return 0;
+//}
 
++(void)clearCache:(NSString *)path{
+    NSFileManager *fileManager=[NSFileManager defaultManager];
+    if ([fileManager fileExistsAtPath:path]) {
+        NSArray *childerFiles=[fileManager subpathsAtPath:path];
+        for (NSString *fileName in childerFiles) {
+            //如有需要，加入条件，过滤掉不想删除的文件
+            NSString *absolutePath=[path stringByAppendingPathComponent:fileName];
+            [fileManager removeItemAtPath:absolutePath error:nil];
+        }
+    }
+    [[SDImageCache sharedImageCache] cleanDisk];
+}
+
+
+// 计算目录大小
+- (CGFloat)folderSizeAtPath:(NSString *)path{
+    // 利用NSFileManager实现对文件的管理
+    NSFileManager *manager = [NSFileManager defaultManager];
+    CGFloat size = 0;
+    if ([manager fileExistsAtPath:path]) {
+        // 获取该目录下的文件，计算其大小
+        NSArray *childrenFile = [manager subpathsAtPath:path];
+        for (NSString *fileName in childrenFile) {
+            NSString *absolutePath = [path stringByAppendingPathComponent:fileName];
+            size += [manager attributesOfItemAtPath:absolutePath error:nil].fileSize;
+        }
+        // 将大小转化为M
+        return size / 1024.0 / 1024.0;
+    }
+    return 0;
+}
+
+-(void)clear{
+    
+    [[SDImageCache sharedImageCache] clearDisk];
+    
+    [[SDImageCache sharedImageCache] clearMemory];
+ 
+    dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+    
+    , ^{
+        
+        NSString *cachPath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+        
+        NSLog(@"%@", cachPath);
+        
+        
+        
+        NSArray *files = [[NSFileManager defaultManager] subpathsAtPath:cachPath];
+        
+        NSLog(@"files :%lu",(unsigned long)[files count]);
+        
+        for (NSString *p in files) {
+            
+            NSError *error;
+            
+            NSString *path = [cachPath stringByAppendingPathComponent:p];
+            
+            if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+                
+                [[NSFileManager defaultManager] removeItemAtPath:path error:&error];
+                
+            }
+            
+        }
+        
+       };
+    
+}
 @end

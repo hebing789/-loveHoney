07003cfbc29d4cd028db5d6e4243c3db0ca11cbc
Flynn_Lee <flynn@qq.com>
2016-10-20 09:52:54 +0800

    订单cell提交

---
+++ loveHoney/loveHoney/Classes/Controllers/MyMenuAndMessage/myMenu/HMDingCell/HMDingCell.m
@@ -0,0 +1,24 @@
+//
+//  HMDingCell.m
+//  loveHoney
+//
+//  Created by 李亚 on 16/10/20.
+//  Copyright © 2016年 hebing. All rights reserved.
+//
+
+#import "HMDingCell.h"
+
+@implementation HMDingCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
